accf1ac26d34c4425151f90467ff74c61a257249
dedislamets <dedi.supatman@modena.co.id>
2020-04-07 13:30:14 +0700

    API 1

---
+++ application/controllers/Api.php
@@ -0,0 +1,82 @@
+<?php
+
+// Default
+// $users = [
+//     ['id' => 0, 'name' => 'John', 'email' => 'john@example.com'],
+//     ['id' => 1, 'name' => 'Jim', 'email' => 'jim@example.com'],
+// ];
+
+// $id = $this->get( 'id' );
+
+// if ( $id === null )
+// {
+//     if ( $users )
+//     {
+//         $this->response( $users, 200 );
+//     }
+//     else
+//     {
+//         $this->response( [
+//             'status' => false,
+//             'message' => 'No users were found'
+//         ], 404 );
+//     }
+// }
+// else
+// {
+//     if ( array_key_exists( $id, $users ) )
+//     {
+//         $this->response( $users[$id], 200 );
+//     }
+//     else
+//     {
+//         $this->response( [
+//             'status' => false,
+//             'message' => 'No such user found'
+//         ], 404 );
+//     }
+// }
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use chriskacerguis\RestServer\RestController;
+
+class Api extends RestController  {
+
+    public function __construct()
+    {
+        parent::__construct();
+        //load model admin
+        $this->load->model('admin');
+    }
+
+    public function login_get()
+    {
+        //get data dari FORM
+        $username = $this->input->get("username");
+        $password = $this->input->get('password');
+
+        //checking data via model
+        $checking = $this->admin->check_login('V_SecAccessGroup', array('PersonalMail' => $username), array('IsPassword' => $password));
+
+        if ($checking != FALSE) {
+            foreach ($checking as $apps) {
+                $session_data = array(
+                    'user_id'   => $apps->RecnumEmployee,
+                    'user_nik'  => $apps->EmployeeId,
+                    'user_name' => $apps->EmployeeName,
+                    'user_mail' => $apps->PersonalMail,
+                );
+
+                $this->response( $session_data, 200 );
+
+            }
+        }else{
+
+            $this->response( [
+                'status' => false,
+                'message' => 'No users were found'
+            ], 404 );
+        }
+        
+    }
+}
