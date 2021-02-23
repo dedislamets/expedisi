4a38e69bc0d5a661357bda7a912eddb1c3111283
dedislamets <dedi.supatman@modena.co.id>
2021-02-23 20:55:44 +0700

    Role User

--- application/controllers/RoleUsers.php
+++ application/controllers/RoleUsers.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class RoleUsers extends CI_Controller {
+class Roleusers extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
