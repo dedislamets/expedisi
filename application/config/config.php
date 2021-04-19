af24964fdbea5fa76846b1842d14b971171ac335
dedislamets <dedi.supatman@modena.co.id>
2021-04-19 22:11:32 +0700

    redirect wwww2

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost:8080/expedisi';
-$config['base_url'] = 'https://wmlogistics.id/esys/';
+$config['base_url'] = 'https://www.wmlogistics.id/esys/';
 
 /*
 |--------------------------------------------------------------------------
