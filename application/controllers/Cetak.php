6564e949a67a11d002e849fc24b76420362747b8
dedislamets <dedi.slamets@gmail.com>
2022-03-10 10:54:36 +0700

    dghrthrheb

--- application/controllers/Cetak.php
+++ application/controllers/Cetak.php
@@ -29,8 +29,8 @@ class Cetak extends CI_Controller {
 
           $data['data_detail'][$key]['spk'] = $routing['spk_no'];
           $data['data_detail'][$key]['pickup_date'] = $routing['pickup_date'];
-          $data['data_detail'][$key]['dari'] = $routing['nama_pengirim'];
-          $data['data_detail'][$key]['tujuan'] = $routing['nama_penerima'];
+          $data['data_detail'][$key]['nama_pengirim'] = $routing['nama_pengirim'];
+          $data['data_detail'][$key]['nama_penerima'] = $routing['nama_penerima'];
           $data['data_detail'][$key]['layanan'] = $routing['moda_name'];
 
           $data['data_detail'][$key]['nama_barang'] = $item['nama_barang'];
